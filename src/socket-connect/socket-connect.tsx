883e0b089ac494a8591bab2e3cc74e216cb3fc41
Alik_Avizha <aleksandravizha@icloud.com>
2023-09-13 14:34:24 +0300

    fix: socket-connect

--- src/socket-connect/socket-connect.tsx
+++ src/socket-connect/socket-connect.tsx
@@ -7,6 +7,10 @@ interface ChatMessage {
     message: string;
 }
 
+// const socket = io('https://back-psi-brown.vercel.app', {
+//     path: '/api/socket.io',
+// });
+
 const socket = io('https://back-psi-brown.vercel.app', {
     path: '/api/socket.io',
 });
